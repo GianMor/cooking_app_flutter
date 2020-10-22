bf74d5bf35152bbfbc7bef2c21a59e65d8537619
Gianluca Moratto <gianluca.moratto@beantech.it>
2020-10-22 11:44:40 +0200

    add route to categorymealscreen

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,3 +1,4 @@
+import 'package:cooking_app_flutter/category_meals_screen.dart';
 import 'package:flutter/material.dart';
 
 class CategoryItem extends StatelessWidget {
@@ -6,20 +7,30 @@ class CategoryItem extends StatelessWidget {
 
   const CategoryItem({this.title, this.color});
 
+  void selectategory(BuildContext ctx) {
+    Navigator.of(ctx)
+        .push(MaterialPageRoute(builder: (_) => CategoryMealsScreen()));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(15),
-      child: Text(
-        title,
-        style: Theme.of(context).textTheme.headline6,
+    return InkWell(
+      onTap: () => selectategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              color.withOpacity(0.7),
+              color,
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+            borderRadius: BorderRadius.circular(15)),
       ),
-      decoration: BoxDecoration(
-          gradient: LinearGradient(colors: [
-            color.withOpacity(0.7),
-            color,
-          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-          borderRadius: BorderRadius.circular(15)),
     );
   }
 }
---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("The recipes")),
+        body: Center(child: Text("Recipes category")));
+  }
+}
