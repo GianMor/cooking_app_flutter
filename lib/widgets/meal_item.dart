edf1e4d2c7a42233c79bccabfd2c468ffd3d5698
Gianluca Moratto <gianluca.moratto@beantech.it>
2020-10-23 09:59:44 +0200

    finsh meal item

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -15,7 +15,42 @@ class MealItem extends StatelessWidget {
       @required this.complexity,
       @required this.affordability});
 
+      String get complexityText {
+        switch (complexity) {
+          case Complexity.Simple:
+            return 'Simple';
+            break;
+          case Complexity.Medium:
+            return 'Medium';
+            break;
+          case Complexity.Hard:
+            return 'Hard';
+            break;
+          default: 
+            return 'Unknown';
+        }
+      }
+
+      String get affordabilityText {
+        switch (affordability) {
+          case Affordability.Affordable:
+            return 'Affordable';
+            break;
+          case Affordability.Pricey:
+            return 'Pricey';
+            break;
+          case Affordability.Luxurious:
+            return 'Luxurious';
+            break;
+          default: 
+            return 'Unknown';
+        }
+      }
+
+
   void selectMeal() {}
+
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -37,8 +72,52 @@ class MealItem extends StatelessWidget {
                   width: double.infinity,
                   fit: BoxFit.cover,
                 ),
+              ),
+              Positioned(
+                bottom: 10,
+                right: 10,
+                child: Container(
+                  width: 300,
+                  color: Colors.black54,
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  child: Text(
+                    title,
+                    style: TextStyle(fontSize: 26, color: Colors.white),
+                    softWrap: true,
+                    overflow: TextOverflow.fade,
+                  ),
+                ),
               )
-            ])
+            ]),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                    Icon(Icons.schedule),
+                    SizedBox(width: 6,),
+                    Text('$duration min'),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                    Icon(Icons.work),
+                    SizedBox(width: 6,),
+                    Text(complexityText),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                    Icon(Icons.attach_money),
+                    SizedBox(width: 6,),
+                    Text(affordabilityText),
+                    ],
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
